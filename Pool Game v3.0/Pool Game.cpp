5ef47901c84217e82d3b625a85dc39d1f49921b0
Megan Cooper <58695342+MegRCooper@users.noreply.github.com>
2023-01-23 15:57:28 +0000

    Init

---
+++ Pool Game v3.0/Pool Game.cpp
@@ -0,0 +1,465 @@
+#include "stdafx.h"
+#include "stdafx.h"
+#include <glut.h>
+#include <math.h>
+#include "simulation.h"
+
+
+// Aim Line variables:
+float gAimAngle = 0.0;
+float gAimPower = 0.25;
+bool gAimControl[4] = { false,false,false,false };
+float gAimAngleSpeed = 2.0f; //radians per second
+float gAimPowerSpeed = 0.25f;
+float gAimPowerMax = 0.75;
+float gAimPowerMin = 0.1;
+float gPlayerStoneFactor = 8.0;
+bool gDoAim = true;
+
+// Camera variables:
+vec3 gCamPos(0.0, 0.7, 2.25);
+vec3 gCamLookAt(0.0, 0.0, 0.0);
+bool gCamRotate = true;
+float gCamRotSpeed = 0.2;
+float gCamMoveSpeed = 0.5;
+bool gCamL = false;
+bool gCamR = false;
+bool gCamU = false;
+bool gCamD = false;
+bool gCamZin = false;
+bool gCamZout = false;
+
+// Rendering Options:
+#define DRAW_SOLID	(0)
+
+/**
+	VOID DOCAMERA:
+	This funxtion rotates the camera around its center point.
+	It takes an int value [ms], which represents milliseconds.
+	The first line of code creates a variable named up stores three vars [0.0, 1.0, and 0.0]
+	respectively for x-, y- and z-axis coordinates. (represents the direction towards the top of the screen).
+
+	If gCamRotate is true, then vec3 camDir = (gCamLookAt - gCamPos).Normalized()
+	if gCamL is true will be carried out, and camDir will have a value between -1 and 1, depending on whether you are looking at your character's left or right side;
+	This indicates that the distance from your character's center point will be represented by a positive number when looking leftward from their position,
+	while the distance from their center point will be represented by a negative number when looking rightward; As long as they stay within those bounds,
+	these values can range from -1 to 1, but since our program only requires one type of value to be returned by this function, we'll use integers instead.
+
+	 SIMPLE EXPLANATION:
+	 The code is used to rotate the camera.
+	 The first part of the code rotates the camera around its centre point, which is gCamPos.
+	 The second part of the code rotates the camera around its right-hand side, which is gCamR.
+	 The third part of the code rotates the camera around its left-hand side, which is gCamL.
+**/
+
+void DoCamera(int ms) {
+	static const vec3 up(0.0, 1.0, 0.0);
+	if (gCamRotate) {
+		if (gCamL) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localL = up.Cross(camDir);
+			vec3 inc = (localL * ((gCamRotSpeed * ms) / 1000.0));
+			gCamLookAt = gCamPos + camDir + inc;
+		}
+
+		if (gCamR) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localR = up.Cross(camDir);
+			vec3 inc = (localR * ((gCamRotSpeed * ms) / 1000.0));
+			gCamLookAt = gCamPos + camDir - inc;
+		}
+
+		if (gCamU) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localR = camDir.Cross(up);
+			vec3 localUp = localR.Cross(camDir);
+			vec3 inc = (localUp * ((gCamMoveSpeed * ms) / 1000.0));
+			gCamLookAt = gCamPos + camDir + inc;
+		}
+
+		if (gCamD) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localR = camDir.Cross(up);
+			vec3 localUp = localR.Cross(camDir);
+			vec3 inc = (localUp * ((gCamMoveSpeed * ms) / 1000.0));
+			gCamLookAt = gCamPos + camDir - inc;
+		}
+	}
+	else {
+		if (gCamL) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localL = up.Cross(camDir);
+			vec3 inc = (localL * ((gCamMoveSpeed * ms) / 1000.0));
+			gCamPos += inc;
+			gCamLookAt += inc;
+		}
+
+		if (gCamR) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localR = camDir.Cross(up);
+			vec3 inc = (localR * ((gCamMoveSpeed * ms) / 1000.0));
+			gCamPos += inc;
+			gCamLookAt += inc;
+		}
+
+		if (gCamU) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localR = camDir.Cross(up);
+			vec3 localUp = localR.Cross(camDir);
+			vec3 inc = (localUp * ((gCamMoveSpeed * ms) / 1000.0));
+			gCamPos += inc;
+			gCamLookAt += inc;
+		}
+
+		if (gCamD) {
+			vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+			vec3 localR = camDir.Cross(up);
+			vec3 localDown = camDir.Cross(localR);
+			vec3 inc = (localDown * ((gCamMoveSpeed * ms) / 1000.0));
+			gCamPos += inc;
+			gCamLookAt += inc;
+		}
+	}
+
+	if (gCamZin) {
+		vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+		vec3 inc = (camDir * ((gCamMoveSpeed * ms) / 1000.0));
+		gCamPos += inc;
+		gCamLookAt += inc;
+	}
+
+	if (gCamZout) {
+		vec3 camDir = (gCamLookAt - gCamPos).Normalised();
+		vec3 inc = (camDir * ((gCamMoveSpeed * ms) / 1000.0));
+		gCamPos -= inc;
+		gCamLookAt -= inc;
+	}
+}
+
+/**
+	RENDERSCENE(VOID)
+	Drawing of the stones:
+		The code starts by declaring a variable called stonesOnTarget an int used to keep track of how many times the player has successfully hit the target with a stone.
+	The code then loops through all of the curling stones on the sheet and checks if they have a NULL value for their tTarget field, which means they are not currently targeting anything.
+	If true, then it sets glColor3f(0.0, 0.0, 1.0) and glPushMatrix() before translating them into position relative to where they were originally placed in relation to each other (STONE_RADIUS / 2).
+	It then draws a solid sphere using glutSolidSphere() or wire spheres using glutWireSphere(). - MAY BE CHANGED
+	After drawing these objects, it pops back out of matrix mode and increments stonesOnTarget by one because now there's one more stone on target than when it started looping through all of the
+	curling stones on the sheet again later in its code block after checking if any new ones had been added since last time around.
+	The code iterates through all the stones on the sheet and checks if their target is NULL.
+	If it is, then it draws a red sphere around the stone with its radius of 32 units.
+	If not, then it increments the value of stonesOnTarget by one and draws a blue sphere with its radius of 12 units.
+
+	Drawing of the Players Stone:
+	 Used to draw a solid sphere.
+	 Using the glTranslatef() function to move the sphere from its current position to another point on the screen.
+	 First checks if there are more than one stone left in the target and if so, it will draw them as a group.
+	 If not, then it will draw each stone individually.
+
+	 Drawing of the curlingsheet:
+	 Then it draws a line for each edge of the curling sheet, which is done in two loops.
+		 The first loop draws lines from the start vertex to the end vertex and then back again; this creates an aiming line that will be used later on when calculating where to place stones.
+		 The second loop draws lines from one stone's position to another stone's position, creating a path between them that can be seen as they curl around each other.
+	 Drawing the curling sheet with glBegin(GL_TRIANGLES).
+	 Useing glVertex3f() functions to draw vertices at points 0 and 1 of every triangle in order (0,1), 2 and 3 (2,3), 4 and 5 (4,5), 6 and 7 (6,7) etc., until all triangles have been drawn.
+	 Next it uses glColor3f() functions to set colors for every point on every triangle: red for point 0; green for point 1; blue for point 2 etc.
+		These colors are used later on when drawing stones so that they can be distinguished from one another while still being part of their respective paths.
+	 After this step has completed successfully,
+	 The code is a fragment of code that draws the curling sheet.
+	 The code begins by drawing the background and then, it draws all the stones on the sheet.
+	 The stonePos() function is used to find where each stone is located in relation to the center of the screen.
+
+**/
+
+void RenderScene(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Sets camera
+	glLoadIdentity();
+	gluLookAt(gCamPos(0), gCamPos(1), gCamPos(2), gCamLookAt(0), gCamLookAt(1), gCamLookAt(2), 0.0f, 1.0f, 0.0f);
+
+	// Draws the stone - THIS NEEDS CHANGING OF THE SHAPE AS THESE ARE SPHERICAL 
+	for (int i = 0; i < NUM_STONES; i++) {
+
+		glColor3f(0.0, 0.0, 1.0);
+		glPushMatrix();
+		glTranslatef(gCurlingSheet.stones[i].stonePos(0), (STONE_RADIUS / 2.0), gCurlingSheet.stones[i].stonePos(1));
+		glutSolidSphere(gCurlingSheet.stones[i].radius, 12, 12);
+		glPopMatrix();
+	}
+
+	//Draw the curling Sheet
+	glColor3f(0.0f, 0.5f, 0.5f);
+	for (int i = 0; i < NUM_EDGES; i++) {
+		glBegin(GL_LINE_LOOP);
+		glVertex3f(gCurlingSheet.edges[i].vertices[0](0), 0.0, gCurlingSheet.edges[i].vertices[0](1));
+		glVertex3f(gCurlingSheet.edges[i].vertices[0](0), 0.1, gCurlingSheet.edges[i].vertices[0](1));
+		glVertex3f(gCurlingSheet.edges[i].vertices[1](0), 0.1, gCurlingSheet.edges[i].vertices[1](1));
+		glVertex3f(gCurlingSheet.edges[i].vertices[1](0), 0.0, gCurlingSheet.edges[i].vertices[1](1));
+		glEnd();
+	}
+
+	//Draw the Target.
+	for (int i = 0; i < NUM_RINGS; i++) {
+		glPushMatrix();
+		if (i % 2 == 0) { glColor3f(1.0, 1.0, 1.0); } // White Rings 
+		else { glColor3f(1.0, 0.0, 0.0); } // Red Rings 
+		GLUquadric* quadric;
+		quadric = gluNewQuadric();
+		glTranslatef(gCurlingSheet.rings[i].targetCenter(0), (-0.001 * i), gCurlingSheet.rings[i].targetCenter(1));
+		glRotatef(9 * 10, 1.0f, 0.0f, 0.0f);
+		gluDisk(quadric, 0, TARGET_SPACING * i, 15, 15);
+		gluDeleteQuadric(quadric);
+		glPopMatrix();
+	}
+
+	// Draw the Aiming Line
+	if (gDoAim) {
+		glBegin(GL_LINES);
+		float cuex = sin(gAimAngle) * gAimPower;
+		float cuez = cos(gAimAngle) * gAimPower;
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(gCurlingSheet.stones[gCurlingSheet.currntStone].stonePos(0), (STONE_RADIUS / 2.0f), gCurlingSheet.stones[gCurlingSheet.currntStone].stonePos(1));
+		glVertex3f((gCurlingSheet.stones[gCurlingSheet.currntStone].stonePos(0) + cuex), (STONE_RADIUS / 2.0f), (gCurlingSheet.stones[gCurlingSheet.currntStone].stonePos(1) + cuez));
+		glEnd();
+	}
+	glFlush();
+	glutSwapBuffers();
+}
+
+
+
+// NOT CHANGED ANY OF THE BELOW AS ITS FROM THE BASE CODE PROVIDED IN WORKSHOP WEEK 4:
+void SpecKeyboardFunc(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:
+	{
+		gAimControl[0] = true;
+		break;
+	}
+	case GLUT_KEY_RIGHT:
+	{
+		gAimControl[1] = true;
+		break;
+	}
+	case GLUT_KEY_UP:
+	{
+		gAimControl[2] = true;
+		break;
+	}
+	case GLUT_KEY_DOWN:
+	{
+		gAimControl[3] = true;
+		break;
+	}
+	}
+}
+
+void SpecKeyboardUpFunc(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:
+	{
+		gAimControl[0] = false;
+		break;
+	}
+	case GLUT_KEY_RIGHT:
+	{
+		gAimControl[1] = false;
+		break;
+	}
+	case GLUT_KEY_UP:
+	{
+		gAimControl[2] = false;
+		break;
+	}
+	case GLUT_KEY_DOWN:
+	{
+		gAimControl[3] = false;
+		break;
+	}
+	}
+}
+
+void KeyboardFunc(unsigned char key, int x, int y) {
+	switch (key) {
+	case(13):
+	{
+		if (gDoAim) {
+			vec2 imp((-sin(gAimAngle) * gAimPower * gPlayerStoneFactor),
+				(-cos(gAimAngle) * gAimPower * gPlayerStoneFactor));
+			gCurlingSheet.stones[gCurlingSheet.currntStone].ApplyImpulse(imp);
+			gCurlingSheet.currntStone++;
+
+		}
+		break;
+	}
+	case(27):
+	{
+		for (int i = 0; i < NUM_STONES; i++) {
+			gCurlingSheet.stones[i].Reset();
+		}
+		break;
+	}
+	case(32):
+	{
+		gCamRotate = false;
+		break;
+	}
+	case('z'):
+	{
+		gCamL = true;
+		break;
+	}
+	case('c'):
+	{
+		gCamR = true;
+		break;
+	}
+	case('s'):
+	{
+		gCamU = true;
+		break;
+	}
+	case('x'):
+	{
+		gCamD = true;
+		break;
+	}
+	case('f'):
+	{
+		gCamZin = true;
+		break;
+	}
+	case('v'):
+	{
+		gCamZout = true;
+		break;
+	}
+	}
+}
+
+void KeyboardUpFunc(unsigned char key, int x, int y) {
+	switch (key) {
+	case(32):
+	{
+		gCamRotate = true;
+		break;
+	}
+	case('z'):
+	{
+		gCamL = false;
+		break;
+	}
+	case('c'):
+	{
+		gCamR = false;
+		break;
+	}
+	case('s'):
+	{
+		gCamU = false;
+		break;
+	}
+	case('x'):
+	{
+		gCamD = false;
+		break;
+	}
+	case('f'):
+	{
+		gCamZin = false;
+		break;
+	}
+	case('v'):
+	{
+		gCamZout = false;
+		break;
+	}
+	}
+}
+
+void ChangeSize(int w, int h) {
+	// Prevent a divide by zero, when window is too short (you cant make a window of zero width).
+	if (h == 0) h = 1;
+	float ratio = 1.0 * w / h;
+	// Reset the coordinate system before modifying
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Set the correct perspective.
+	gluPerspective(45, ratio, 0.2, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	//gluLookAt(0.0,0.7,2.1, 0.0,0.0,0.0, 0.0f,1.0f,0.0f);
+	gluLookAt(gCamPos(0), gCamPos(1), gCamPos(2), gCamLookAt(0), gCamLookAt(1), gCamLookAt(2), 0.0f, 1.0f, 0.0f);
+}
+
+void InitLights(void) {
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	GLfloat light_ambient[] = { 2.0, 2.0, 2.0, 1.0 };
+	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void UpdateScene(int ms) {
+	if (gCurlingSheet.AnyStonesMoving() == false) { 
+		gDoAim = true; 
+		gCurlingSheet.stones[gCurlingSheet.currntStone].stonePos(0) = 0;
+		gCurlingSheet.stones[gCurlingSheet.currntStone].stonePos(1) = TABLE_Z - 0.25;
+	}
+	else gDoAim = false;
+	if (gDoAim) {
+		if (gAimControl[0]) gAimAngle -= ((gAimAngleSpeed * ms) / 1000);
+		if (gAimControl[1]) gAimAngle += ((gAimAngleSpeed * ms) / 1000);
+		if (gAimAngle < 0.0) gAimAngle += TWO_PI;
+		if (gAimAngle > TWO_PI) gAimAngle -= TWO_PI;
+		if (gAimControl[2]) gAimPower += ((gAimPowerSpeed * ms) / 1000);
+		if (gAimControl[3]) gAimPower -= ((gAimPowerSpeed * ms) / 1000);
+		if (gAimPower > gAimPowerMax) gAimPower = gAimPowerMax;
+		if (gAimPower < gAimPowerMin) gAimPower = gAimPowerMin;
+	}
+	DoCamera(ms);
+	gCurlingSheet.Update(ms);
+	glutTimerFunc(SIM_UPDATE_MS, UpdateScene, SIM_UPDATE_MS);
+	glutPostRedisplay();
+}
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	gCurlingSheet.SetUpEdges();
+	gCurlingSheet.SetUpRings();
+	gCurlingSheet.stones[0].SetPlayerStone();
+	glutInit(&argc, ((char**)argv));
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(1000, 700);
+	//glutFullScreen();
+	glutCreateWindow("MSc Advanced Programming Assigment: Stone Curling Game (COO19695119)");
+#if DRAW_SOLID
+	InitLights();
+#endif
+	glutDisplayFunc(RenderScene);
+	glutTimerFunc(SIM_UPDATE_MS, UpdateScene, SIM_UPDATE_MS);
+	glutReshapeFunc(ChangeSize);
+	glutIdleFunc(RenderScene);
+
+	glutIgnoreKeyRepeat(1);
+	glutKeyboardFunc(KeyboardFunc);
+	glutKeyboardUpFunc(KeyboardUpFunc);
+	glutSpecialFunc(SpecKeyboardFunc);
+	glutSpecialUpFunc(SpecKeyboardUpFunc);
+	glEnable(GL_DEPTH_TEST);
+	glutMainLoop();
+}
---
+++ Pool Game v3.0/simulation.cpp
@@ -0,0 +1,328 @@
+// Simulation Source File:
+#include "stdafx.h"
+#include "simulation.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// MACROS:
+#define SMALL_VEL (0.01f)
+
+// GLOBALS:
+curlingSheet gCurlingSheet;
+
+float gCoeffRestitution = 0.5f;
+float gCoeffFriction = 0.03f;
+float gGravityAcc = 9.8f;
+
+static const float gRackPositionX[] = { 0.0f, 0.0f,(STONE_RADIUS * 2.0f),(-STONE_RADIUS * 2.0f),(STONE_RADIUS * 4.0f) };
+static const float gRackPositionZ[] = { 0.5f, 0.0f,(-STONE_RADIUS * 3.0f),(-STONE_RADIUS * 3.0f) };
+
+/**
+	EDGE CLASS MEMBERS:
+		Creates two vectors - temp & normal. The first is in the direction of the edge in the X-direction
+		while the second is in the direction of the edge in the Y-direction.
+	It then calcs two new vectors and normalises and centres - normailises scales both vectors so they have a len of 1.
+	Centre calcs where on the screen [0,0], should be placed for each vec, based on their respective lens and angles.
+**/
+void edge::MakeNormal(void) {
+	// Provided - can do in 2-D
+	vec2 tempVertices = vertices[1] - vertices[0];
+	normal(0) = tempVertices(1);
+	normal(1) = -tempVertices(0);
+	normal.Normalise();
+}
+
+void edge::MakeCentre(void) {
+	centre = vertices[0];
+	centre = vertices[1];
+	centre /= 2.0;
+}
+
+// Player class Members:
+int player::playerIndxCnt = 0;
+
+/**
+	STONE CLASS MEMBERS:
+		Defines two variables - rowIndex [stores the current row number], row [stores the current column number.]
+	Starts by setting the velocity to Zero [IMPORTANT - It'll help keep the stines in place as they're drawn on screen].
+	Iteration through the rows in turn - Starts at rowOne works down to rowZero:
+		For each row it calcs how far away from (and to) RowOne (the starting point), that psrticular row is located.
+		This info is then used to calc where each stone should be pos onscreen. Final line sets up the pos of all stones in the game.
+		It calculates the row and column coordinates of each stone based on its current position and radius.
+		Updates the pos of each stone based on these Coords.
+**/
+
+int stone::stoneIndxCnt = 0;
+void stone::Reset(void) {
+	velocity = 0.0;
+	if (indx == 0) {
+		stonePos(1) = TABLE_Z - 0.25;
+		stonePos(0) = 0;
+	}
+	else {
+		stonePos(1) = TABLE_Z + (0.1 * indx);
+		stonePos(0) = -0.5;
+	}
+
+}
+
+void stone::ApplyImpulse(vec2 imp) {
+	velocity = imp;
+}
+
+void stone::ApplyFrictionForce(int ms) {
+	if (velocity.Magnitude() <= 0.0) return;
+	// Acceleration is opposite to the direction of Motion:
+	vec2 accelaration = -velocity.Normalised();
+	// F=(MA), thus acceleration = force/mass = constant*g
+	accelaration *= (gCoeffFriction * gGravityAcc);
+	// Integrate velocity [find the change in V].
+	vec2 velChange = ((accelaration * ms) / 1000.0f);
+	// Cap magnitude of change in velocity to remove intergration errs.
+	if (velChange.Magnitude() > velocity.Magnitude())
+		velocity = 0.0;
+	else
+		velocity += velChange;
+}
+
+void stone::DoStoneCollision(stone& s) {
+	if (HasHitStone(s))
+		HitStone(s);
+}
+
+void stone::DoPlaneCollision(const edge& e) {
+	if (HasHitPlane(e))
+		HitPlane(e);
+}
+
+void stone::DoTargetCollision(target& t) {
+
+}
+
+void stone::Update(int ms) {
+	// Applying Friction:
+	ApplyFrictionForce(ms);
+	stonePos += ((velocity * ms) / 1000.f);
+	// Setting small velocities to zero.
+	if (velocity.Magnitude() < SMALL_VEL)
+		velocity = 0.0;
+}
+
+bool stone::HasHitPlane(const edge& e) const {
+	if (tTarget != NULL)
+		return false;
+	// If moving away from plane, cannot hit.
+	if (velocity.Dot(e.normal) >= 0.0)
+		return false;
+	// If in front of the Plane, then haven't hit:
+	vec2 relPos = stonePos - e.vertices[0];
+	double sep = relPos.Dot(e.normal);
+	if (sep > radius)
+		return false;
+	return true;
+}
+
+//NOT SURE THIS IS NEEDED AS IT IS FROM THE OG CODE - decide if required and adjust as needed!!
+bool stone::HasHitTarget(const target& t) const {
+	// work out relative position of stone from target,	distance between stone and target and relative velocity
+	vec2 relPosn = stonePos - t.targetCenter;
+	float dist = (float)relPosn.Magnitude();
+	vec2 relPosnNorm = relPosn.Normalised();
+
+	//if moving away from plane, cannot hit
+	if (velocity.Dot(relPosnNorm) >= 0.0) return false;
+	//if distnce is more than sum of radi, have not hit
+	if (dist > (radius + t.targetRad)) return false;
+	return true;
+}
+
+bool stone::HasHitStone(const stone& s) const {
+	// Works out the relative pos od stone from other stones, distance between stones and relative velocity
+	vec2 relPos = stonePos - s.stonePos;
+	float dist = (float)relPos.Magnitude();
+	vec2 relPosNorm = relPos.Normalised();
+	vec2 relVel = velocity - s.velocity;
+	// Moving apart cannot hit:
+	if (relVel.Dot(relPosNorm) >= 0.0)
+		return false;
+	// If distnce is more than sum of radii, have not hit
+	if (dist > (radius + s.radius))
+		return false;
+	return true;
+}
+
+void stone::HitPlane(const edge& e) {
+	// Reverse velocity component perpendicular to plane  
+	double comp = velocity.Dot(e.normal) * (1.0 + gCoeffRestitution);
+	vec2 delta = -(e.normal * comp);
+	velocity += delta;
+}
+
+
+
+void stone::HitStone(stone& s) {
+	// Find direction from other stone to this stone
+	vec2 relDir = (stonePos - s.stonePos).Normalised();
+
+	/**
+		Split velocities into 2 parts:  one component perpendicular, and one parallel to the collision plane, for both stones.
+		(NB the collision plane is defined by the point of contact and the contact normal).
+	**/
+	float perpV = (float)velocity.Dot(relDir);
+	float perpV2 = (float)s.velocity.Dot(relDir);
+	vec2 parallelV = velocity - (relDir * perpV);
+	vec2 parallelV2 = s.velocity - (relDir * perpV2);
+
+	// Calcs new perpendicular comps.
+	float sumMass = mass + s.mass;
+	float perpVNew = (float)((perpV * (mass - s.mass)) / sumMass) + (float)((perpV2 * (2.0 * s.mass)) / sumMass);
+	float perpVNew2 = (float)((perpV2 * (s.mass - mass)) / sumMass) + (float)((perpV * (2.0 * mass)) / sumMass);
+
+	// Find new velocities by adding unchanged parallel component to new perpendicluar component.
+	velocity = parallelV + (relDir * perpVNew);
+	s.velocity = parallelV2 + (relDir * perpVNew2);
+
+	// Adding of particles for clashes between stones:
+	int randNumParts = (rand() % 5) + 5;
+	vec3 pos(stonePos(0), radius / 2.0, stonePos(1));
+	vec3 oset(relDir(0), 0.0, relDir(1));
+	pos += (oset * radius);
+	for (int i = 0; i < randNumParts; i++) {
+		gCurlingSheet.parts.AddParticle(pos);
+	}
+}
+
+void stone::SetPlayerStone() {
+	isPlayerStone = true;
+}
+
+/**
+	PARTICLE CLASS MEMBERS:
+	The particle position is updated every millisec.
+	partPos += (partVel * ms) / 1000.0; - The first line adds partPos to the current value of partPos and / 1,000 to get a new value for partVel every millisecond.
+	This will cause the velocity of particles to increase over time as they move away from their starting point in space.
+	partVel(1) -= (4.0 * ms) / 1000.0; - The second line subtracts 4 times per millisecond from the current velocity and divides that number by 9800 seconds per minute to get a new velocity 
+	for each particle at each step in time.
+	
+**/
+void particle::update(int ms) {
+	partPos += (partVel * ms) / 1000.0;
+	partVel(1) -= (4.0 * ms) / 1000.0; //(9.8*ms)/1000.0;
+}
+
+/**
+	PARTICLE SET CLASS MEMBERS:
+	 Starts by checking to see if the particleSet has reached its maximum number of particles.
+		If yes - deletes all particles and starts the count at 0.
+	Then a rand value is generated in range (0-200) - this value is used as the velocity for each induvidual particle in the set.
+	 The velocities are multiplied by 2, 1, or -1 depending on which side of the center line they fall on respectively (0-100).
+	 Finally, every time ms milliseconds pass since last update(), 'i' will be incremented by one until it reaches num whereupon 
+	 'i' will stop updating and delete itself from the list so that there are no more updates to do.
+**/
+void particleSet::AddParticle(const vec3& pos) {
+	if (num >= MAX_PARTICLES) return;
+	particles[num] = new particle;
+	particles[num]-> partPos = pos;
+
+	particles[num]->partVel(0) = ((rand() % 200) - 100) / 200.0;
+	particles[num]->partVel(2) = ((rand() % 200) - 100) / 200.0;
+	particles[num]->partVel(1) = 2.0 * ((rand() % 100) / 100.0);
+
+	num++;
+}
+
+void particleSet::update(int ms) {
+	int i = 0;
+	while (i < num) {
+		particles[i]->update(ms);
+		if ((particles[i]->partPos(1) < 0.0) && (particles[i]->partVel(1) < 0.0)) {
+			delete particles[i];
+			particles[i] = particles[num - 1];
+			num--;
+		}
+		else i++;
+	}
+}
+
+/**
+	CURLINGSHEET CLASS MEMBERS:
+	Starts by setting up the four edges of the curling sheet.
+		First edge is set to have vertices [-TABLE_X, -TABLE_Z].
+		Second edge is set to have vertices [0,0].
+		Third edge is set to have vertices [1,1].
+		Fourth edge is set to have verticies [-TABLE_X, TABLE_Z].
+	It then loops through all of the edges setting up the vertices using two variables; one for the x value,
+	the other for the z - it sets these variables to be equal to -TABLE_X and TABLE_Z, respectively.
+		This ensures that each vertex will be located on either side of the center line
+	Finally, it writes these values out to memory so that they can be used later when drawing graphics.
+	Variables declared as i and j - which stores the current pos of the left most and right most stones, respectivley.
+	It then checks to see if any of the stones have collided with each other.
+	The code first sets up a variable to store the number of stones on the curling sheet, as well as the number of rings.
+	It then checks for any collisions between each stone and the rings.
+	If there are any collisions, the code will then proceed to update each individual stone's position.
+
+		ADD TO WITH A FINISHED DEFINITIONS
+**/
+
+void curlingSheet::SetUpEdges(void) {
+	edges[0].vertices[0](0) = -TABLE_X;
+	edges[0].vertices[0](1) = -TABLE_Z;
+	edges[0].vertices[1](0) = -TABLE_X;
+	edges[0].vertices[1](1) = TABLE_Z;
+
+	edges[1].vertices[0](0) = -TABLE_X;
+	edges[1].vertices[0](1) = TABLE_Z;
+	edges[1].vertices[1](0) = TABLE_X;
+	edges[1].vertices[1](1) = TABLE_Z;
+
+	edges[2].vertices[0](0) = TABLE_X;
+	edges[2].vertices[0](1) = TABLE_Z;
+	edges[2].vertices[1](0) = TABLE_X;
+	edges[2].vertices[1](1) = -TABLE_Z;
+
+	edges[3].vertices[0](0) = TABLE_X;
+	edges[3].vertices[0](1) = -TABLE_Z;
+	edges[3].vertices[1](0) = -TABLE_X;
+	edges[3].vertices[1](1) = -TABLE_Z;
+
+	for (int i = 0; i < NUM_EDGES; i++) {
+		edges[i].MakeCentre();
+		edges[i].MakeNormal();
+	}
+}
+
+void curlingSheet::SetUpRings(void) {
+	for (int i = 0; i < NUM_RINGS; i++) {
+		rings[i].targetCenter(0) = 0;
+		float zLoc = (TABLE_X * 2) - (TARGET_SPACING * NUM_RINGS);
+		rings[i].targetCenter(1) = zLoc - TABLE_Z;
+	}
+}
+
+
+void curlingSheet::Update(int ms) {
+
+	// Checks for collisions for each stone
+	for (int i = 0; i < NUM_STONES; i++) {
+		for (int j = (i + 1); j < NUM_RINGS; j++) {
+			stones[i].DoTargetCollision(rings[j]);
+		}
+		for (int j = 0; j < NUM_EDGES; j++) {
+			stones[i].DoPlaneCollision(edges[j]);
+		}
+		for (int j = (i + 1); j < NUM_STONES; j++) {
+			stones[i].DoStoneCollision(stones[j]);
+		}
+	}
+	// Update all stones:
+	for (int i = 0; i < NUM_STONES; i++) stones[i].Update(ms);
+}
+
+bool curlingSheet::AnyStonesMoving(void) const {
+	// Return true if any stone has a non-zero velocity.
+	for (int i = 0; i < NUM_STONES; i++) {
+		if (stones[i].velocity(0) != 0.0) return true;
+		if (stones[i].velocity(1) != 0.0) return true;
+	}
+	return false;
+}
